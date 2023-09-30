ba74785a01c48c7569e31a70a30a59f1c666be6f
abhijitWebDev <110275876+abhijitWebDev@users.noreply.github.com>
2023-09-30 09:00:16 +0530

    github data added

--- index.js
+++ index.js
@@ -3,6 +3,41 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const githubData = {
+  "login": "abhijitWebDev",
+  "id": 110275876,
+  "node_id": "U_kgDOBpKtJA",
+  "avatar_url": "https://avatars.githubusercontent.com/u/110275876?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/abhijitWebDev",
+  "html_url": "https://github.com/abhijitWebDev",
+  "followers_url": "https://api.github.com/users/abhijitWebDev/followers",
+  "following_url": "https://api.github.com/users/abhijitWebDev/following{/other_user}",
+  "gists_url": "https://api.github.com/users/abhijitWebDev/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/abhijitWebDev/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/abhijitWebDev/subscriptions",
+  "organizations_url": "https://api.github.com/users/abhijitWebDev/orgs",
+  "repos_url": "https://api.github.com/users/abhijitWebDev/repos",
+  "events_url": "https://api.github.com/users/abhijitWebDev/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/abhijitWebDev/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": null,
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 7,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 10,
+  "created_at": "2022-07-30T05:26:33Z",
+  "updated_at": "2023-09-17T08:01:35Z"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -17,6 +52,10 @@ app.get('/login', (req,res) => {
 
 app.get('/youtube', (req, res) => {
 res.send('<h2>Chai aur Code </h2>')
+})
+
+app.get('/github', (req,res) => {
+  res.json(githubData);
 })
 
 app.listen(process.env.PORT, () => {
